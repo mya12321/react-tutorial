3edd7c6131dcc94b31062fa4504dfde4109f8ed7
Andy <mya12321@gmail.com>
2018-04-25 17:42:56 +0800

    finish tutorial on website

--- src/app/board.jsx
+++ src/app/board.jsx
@@ -3,16 +3,9 @@ import React from 'react';
 import Square from './square';
 
 export default class Board extends React.Component {
-  renderSquare() {
-    return <Square/>;
-  }
-
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -30,5 +23,13 @@ export default class Board extends React.Component {
         </div>
       </div>
     );
+  }
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.handleClick(i)}
+      />);
   }
 }
