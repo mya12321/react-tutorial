1203ff8c8aec012d6ea8a8a6f8f2e01a46d37229
Andy Miao <mya12321@gmail.com>
2019-06-07 11:28:26 +0800

    Updates

---
+++ precache-manifest.25081cdb7b69f6cc4bd4f358492fc7be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79263f67a6a2a96638704e804e59ee7a",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "9180950cc5e40d74f205",
+    "url": "/react-tutorial/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "9a5c0cc5b7b5942488d9",
+    "url": "/react-tutorial/static/js/2.56cb80cd.chunk.js"
+  },
+  {
+    "revision": "9180950cc5e40d74f205",
+    "url": "/react-tutorial/static/js/main.7ca20c88.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
